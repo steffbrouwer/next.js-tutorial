0ab6b0c042bf133a0dd047d9a54562ec8a6e8c2e
steffbrouwer <sbrouwer05@gmail.com>
2024-12-04 01:28:14 +0100

    ep -18

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,24 @@
-export default function ProductDetails( { 
-    params, 
-}: {
-    params: { productId: string};
-}) {
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string;
+    };
+};
+
+export const generateMetadata = async ({
+     params,
+    }: Props): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Iphone ${params.productId}`);
+        }, 100);
+    });
+    return {
+        title: `Product ${title}`,
+    };
+};
+
+export default function ProductDetails( { params }: Props) {
     return <h1>Details about product { params.productId}</h1>;
 }
